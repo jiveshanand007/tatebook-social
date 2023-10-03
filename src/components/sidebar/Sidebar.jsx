15c764fe797643750df3b0d4104431d46e6583aa
jiveshanand007 <jiveshanand007@gmail.com>
2023-10-04 04:41:59 +0530

    feed share added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.css"
-import { RssFeed } from '@mui/icons-material';
+import { RssFeed,Chat,PlayCircle,Groups,Bookmarks,HelpOutline,Work,Event,School } from '@mui/icons-material';
 export default function Sidebar() {
     return (
         <div className="sidebar">
@@ -11,43 +11,43 @@ export default function Sidebar() {
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <Chat className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chat</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <PlayCircle className="sidebarIcon" />
+                        <span className="sidebarListItemText">Videos</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <Groups className="sidebarIcon" />
+                        <span className="sidebarListItemText">Groups</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <Bookmarks className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <HelpOutline className="sidebarIcon" />
+                        <span className="sidebarListItemText">Questions</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <Work className="sidebarIcon" />
+                        <span className="sidebarListItemText">Jobs</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <Event className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
 
                     </li>
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon" />
-                        <span className="sidebarListItemText">Feed</span>
+                        <School className="sidebarIcon" />
+                        <span className="sidebarListItemText">Courses</span>
 
                     </li>
                 </ul>
