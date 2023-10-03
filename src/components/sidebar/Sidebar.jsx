31a6fc5c34ac86256ea98104b551887fc28e6759
jiveshanand007 <jiveshanand007@gmail.com>
2023-10-03 21:15:29 +0530

    sidebar added

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,117 @@
+import "./sidebar.css"
+import { RssFeed } from '@mui/icons-material';
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr"/>
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Selmon Bhoi</span>
+                    </li>   
+                </ul>
+            </div>
+        </div>
+    )
+}
