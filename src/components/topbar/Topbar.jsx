c9e7ae211b03b11fd72b5bc62f80e30d6307590e
jiveshanand007 <jiveshanand007@gmail.com>
2023-10-03 17:35:01 +0530

    navbar added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,43 +1,41 @@
 import React from 'react'
 import './topbar.css'
-import {Person, Search,Chat,Notifications} from '@mui/icons-material'
-
-
+import { Person, Search, Chat, Notifications } from '@mui/icons-material'
 
 export default function Topbar() {
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
                 <span className="logo">TateBook</span>
-            </div>  
+            </div>
             <div className="topbarCenter">
                 <div className="searchBar">
-                    <Search/>
+                    <Search className="searchIcon" />
                     <input placeholder="Search for friend,post or video" className="searchInput" />
                 </div>
 
-            </div>  
+            </div>
             <div className="topbarRight">
                 <div className="topbarLinks">
-                    <span className="topbarLink">HomePage</span>
+                    <span className="topbarLink">Homepage</span>
                     <span className="topbarLink">Timeline</span>
                 </div>
                 <div className="topbarIcons">
                     <div className="topbarIconItem">
-                        <Person/>
+                        <Person />
                         <span className="topbarIconBadge">1</span>
                     </div>
                     <div className="topbarIconItem">
-                        <Chat/>
-                        <span className="topbarIconBadge">1</span>
+                        <Chat />
+                        <span className="topbarIconBadge">99</span>
                     </div>
                     <div className="topbarIconItem">
-                        <Notifications/>
+                        <Notifications />
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
                 <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
-            </div>  
+            </div>
         </div>
     )
 }
