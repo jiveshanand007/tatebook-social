8a5018f3672a9dc3280fc7dde9ecb0bc9bb52df1
jiveshanand007 <jiveshanand007@gmail.com>
2023-10-04 19:40:25 +0530

    rightbar added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,73 @@ import "./rightbar.css"
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="/assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText"><b>Sharukh</b> and <b>2 others</b> have their Birthday Today</span>
+                </div>
+                <img src="/assets/person/5.jpeg" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/2.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Halelueya</span>
+                    </li>
+                </ul>
+
+            </div>
         </div>
     )
 }
